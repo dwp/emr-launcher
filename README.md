35fbb70278edebf498fe20145c49a854ad01a4b5
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2020-04-01 09:28:59 +0100

    Initial commit; completely untested (#1)

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # emr-launcher
 Lambda based EMR Cluster Launcher
+
+## What does it do?
+
+Given a set of YAML files that specify an EMR cluster configuration, it will
+call the EMR API to generate that cluster.
+
+## How do I run it locally?
+
+emr-launcher can easily be run from the command line:
+
+```
+pipenv install
+EMR_LAUNCHER_CONFIG_DIR=<path to config files> ./emr_launcher.py
+```
+
+`EMR_LAUNCHER_CONFIG_DIR` must point to a directory containing YAML files defining the configuration of the desired EMR cluster. See below for details.
+
+## How do I run it as a Lambda function?
+
+The Lambda function needs 2 environment variables set:
+
+`EMR_LAUNCHER_CONFIG_S3_BUCKET` - the bucket that contains your YAML configuration files
+`EMR_LAUNCHER_CONFIG_S3_FOLDER` - the S3 folder location containing your YAML configuration files
+
+## How do I write the configuration files
+
+Configuration is via a series of YAML files. The easiest way to get started is
+to look at the [examples](docs/examples/).
+
+The only hard requirements are that you **must** have files called
+`cluster.yaml` and `instances.yml` in the location specified by
+`EMR_LAUNCHER_CONFIG_DIR`.
+
+You can optionally also place a `steps.yaml` file in that same location.
+
+Any other files will be ignored.
+
+## Developing locally
+
+The recommended way to hack on `emr-launcher` locally is within a virtual
+environment via `pipenv`. This can easily be done by running the following
+from this top-level directory:
+
+```
+make bootstrap
+pipenv install
+```
+
+Remember to run `pipenv shell` to reactivate your virtual environment each time
+you enter a new terminal session!
---
+++ emr_launcher.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import logging
+import os
+
+import boto3
+import yaml
+from pythonjsonlogger import jsonlogger
+
+
+def configure_log():
+    """Configure JSON logger."""
+    log_level = os.environ.get("EMR_LAUNCHER_LOG_LEVEL", "INFO").upper()
+    numeric_level = getattr(logging, log_level, None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: %s" % log_level)
+    logging.basicConfig(level=log_level)
+    logger = logging.getLogger("emr_launcher")
+    logger.propagate = False
+    console_handler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter(
+        "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+    )
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+def read_s3_config(bucket: str, key: str, required: bool = True) -> dict:
+    config = {}
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        config = yaml.safe_load(response["Body"])
+    except:
+        raise
+
+    return config
+
+
+def read_local_config(config_file: str, required: bool = True) -> dict:
+    config = {}
+    try:
+        with open(config_file, "r") as in_file:
+            config = yaml.safe_load(in_file)
+    except FileNotFoundError:
+        if required:
+            raise
+    except:
+        raise
+
+    return config
+
+
+def read_config(config_type: str, required: bool = True) -> dict:
+    """Reads an EMR cluster configuration file.
+
+    Reads configuration details of an EMR cluster from either a local file or
+    from an S3 object.
+
+    Parameters:
+    config_type (str): The type of config file to read. Must be one of
+                       `cluster`, `instances`, or `steps`.
+
+    required (bool): Whether or not the configuration file should be required
+                     to be present. If set to True and the configuration file
+                     can't be read, then this function will raise an exception
+    Returns:
+    dict: A dictionary containing the cluster configuration parsed from the
+          provided input.
+    """
+    logger = logging.getLogger("emr_launcher")
+    config = {}
+    local_config_dir = os.getenv("EMR_LAUNCHER_CONFIG_DIR")
+
+    if local_config_dir:
+        logger.info("Locating configs", extra={"local_config_dir": {local_config_dir}})
+        config = read_local_config(
+            os.path.join(local_config_dir, f"{config_type}.yaml"), required
+        )
+    else:
+        s3_bucket = os.getenv("EMR_LAUNCHER_CONFIG_S3_BUCKET")
+        s3_folder = os.getenv("EMR_LAUNCHER_CONFIG_S3_FOLDER")
+        logger.info(
+            "Locating configs", extra={"s3_bucket": s3_bucket, "s3_folder": s3_folder}
+        )
+        s3_key = f"{s3_folder}/{config_type}.yaml"
+        config = read_s3_config(s3_bucket, s3_key, required)
+
+    logger.debug(f"{config_type} config:", extra=config)
+    return config
+
+
+def launch_cluster(event: dict = {}, context: object = None) -> dict:
+    """Launches an EMR cluster with the provided configuration."""
+    logger = configure_log()
+
+    cluster_config = read_config("cluster")
+
+    cluster_config.update(read_config("instances"))
+    cluster_config.update(read_config("steps", False))
+    logger.debug("Requested cluster parameters", extra=cluster_config)
+
+    # logger.info("Submitting cluster creation request")
+    # emr = boto3.client("emr")
+    # resp = emr.run_job_flow(**cluster_config)
+    # logger.info("Cluster submission successful", extra=resp)
+
+    # return resp
+
+
+if __name__ == "__main__":
+    launch_cluster()
