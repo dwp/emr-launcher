8f3c14b10b87cd89ce42b16e8839beccb33b2017
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2020-09-16 15:16:46 +0100

    DW-4929-Move-RDS-Password-to-Secerts

--- emr_launcher.py
+++ emr_launcher.py
@@ -9,6 +9,19 @@ import ast
 import re
 from pythonjsonlogger import jsonlogger
 
+def retrieve_secrets(secret_name):
+    try:
+        secret_value = ''
+        session = boto3.session.Session()
+        client = session.client(service_name="secretsmanager")
+        response = client.get_secret_value(SecretId=secret_name)
+        response_string = response["SecretString"]
+        response_dict = ast.literal_eval(response_string)
+        secret_value = response_dict['password']
+    except Exception as e:
+        logging.info(secret_name + " Secret not found in secretsmanager")
+    return secret_value
+
 
 def configure_log():
     """Configure JSON logger."""
@@ -43,7 +56,6 @@ def read_s3_config(bucket: str, key: str, required: bool = True) -> dict:
         )
     except:
         raise
-
     return yaml.safe_load(config)
 
 
@@ -57,7 +69,7 @@ def read_local_config(config_file: str, required: bool = True) -> dict:
             raise
     except:
         raise
-    return config
+    return yaml.safe_load(config)
 
 
 def read_config(config_type: str, required: bool = True) -> dict:
@@ -106,6 +118,10 @@ def handler(event: dict = {}, context: object = None) -> dict:
     cluster_config = read_config("cluster")
 
     cluster_config.update(read_config("configurations", False))
+    secret_name = cluster_config['Configurations'][3]['Properties']["javax.jdo.option.ConnectionPassword"]
+    secret_value = retrieve_secrets(secret_name)
+    cluster_config['Configurations'][3]['Properties']["javax.jdo.option.ConnectionPassword"]=secret_value
+    cluster_config['Configurations'][4]['Properties']["javax.jdo.option.ConnectionPassword"]=secret_value
     cluster_config.update(read_config("instances"))
     cluster_config.update(read_config("steps", False))
     logger.debug("Requested cluster parameters", extra=cluster_config)
