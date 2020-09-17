81a674567d9ef3e66f970d3db7f4070cf64152e8
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-09-17 16:10:37 +0100

    test correlation_id being  to adg lambda

--- emr_launcher.py
+++ emr_launcher.py
@@ -8,6 +8,7 @@ import yaml
 import ast
 import re
 from pythonjsonlogger import jsonlogger
+import json
 
 
 def configure_log():
@@ -103,7 +104,17 @@ def handler(event: dict = {}, context: object = None) -> dict:
     """Launches an EMR cluster with the provided configuration."""
     logger = configure_log()
 
+    sns_message = event["Records"][0]["Sns"]
+    payload = json.loads(sns_message["Message"])
+    correlation_id = payload["correlation_id"]
+
     cluster_config = read_config("cluster")
+    # Obtain Spark arguments for EMR from the config and add correlation id to it
+    sparks_args = cluster_config['Steps'][2]['HadoopJarStep']['Args']
+    sparks_args.append("--correlation_id")
+    sparks_args.append(correlation_id)
+    cluster_config['Steps'][2]['HadoopJarStep']['Args'] = sparks_args
+    logger.debug("Requested cluster parameters", extra=cluster_config)
 
     cluster_config.update(read_config("configurations", False))
     cluster_config.update(read_config("instances"))
@@ -121,4 +132,9 @@ def handler(event: dict = {}, context: object = None) -> dict:
 
 
 if __name__ == "__main__":
-    handler()
+    logger = configure_log()
+    try:
+        json_content = json.loads(open("event.json", "r").read())
+        handler(json_content, None)
+    except Exception as e:
+        logger.error(e)
