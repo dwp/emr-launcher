2c30314adc15d8dd0333b13ec346c43dbbd33813
sandeepg-github <64797279+sandeepg-github@users.noreply.github.com>
2021-02-10 16:19:21 +0000

    DW-5577 update ADG cluster name (#37)
    * DW-5577 update ADG cluster name
    
    * Fixup: Format Python code with Black
    
    Co-authored-by: DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>

--- emr_launcher/handler.py
+++ emr_launcher/handler.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
-import ast
 import json
-import logging
-import os
-from dataclasses import dataclass
 
-import boto3
-import yaml
-
+from emr_launcher.ClusterConfig import ClusterConfig
+from emr_launcher.aws import sm_retrieve_secrets, emr_launch_cluster
 from emr_launcher.logger import configure_log
 from emr_launcher.util import (
     read_config,
@@ -18,12 +13,13 @@ from emr_launcher.util import (
     add_command_line_params,
     adg_trim_steps_for_incremental,
 )
-from emr_launcher.aws import sm_retrieve_secrets, emr_launch_cluster
-from emr_launcher.ClusterConfig import ClusterConfig
 
 PAYLOAD_S3_PREFIX = "s3_prefix"
 PAYLOAD_CORRELATION_ID = "correlation_id"
 PAYLOAD_SNAPSHOT_TYPE = "snapshot_type"
+ADG_NAME = "analytical-dataset-generator"
+SNAPSHOT_TYPE_FULL = "full"
+SNAPSHOT_TYPE_INCREMENTAL = "incremental"
 
 
 def build_config(
@@ -235,6 +231,10 @@ def old_handler(event=None) -> dict:
         )
         adg_trim_steps_for_incremental(cluster_config, snapshot_type)
 
+    # Renaming ADG cluster based on snapshot type full/incremental
+    cluster_name = cluster_config["Name"]
+    if cluster_name == ADG_NAME:
+        update_adg_cluster_name(cluster_config, snapshot_type)
     logger.debug("Requested cluster parameters", extra=cluster_config)
 
     resp = emr_launch_cluster(cluster_config)
@@ -242,3 +242,11 @@ def old_handler(event=None) -> dict:
     logger.debug(resp)
 
     return resp
+
+
+def update_adg_cluster_name(cluster_config, snapshot_type):
+    cluster_config["Name"] = (
+        f"{ADG_NAME}-{SNAPSHOT_TYPE_INCREMENTAL}"
+        if snapshot_type == SNAPSHOT_TYPE_INCREMENTAL
+        else f"{ADG_NAME}-{SNAPSHOT_TYPE_FULL}"
+    )
