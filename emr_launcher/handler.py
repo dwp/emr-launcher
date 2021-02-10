b83c7f8b505f6f5ec844bcd23ce9abf8537b6600
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-02-10 14:25:00 +0000

    change snapshot type to full
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- emr_launcher/handler.py
+++ emr_launcher/handler.py
@@ -111,14 +111,14 @@ def old_handler(event=None) -> dict:
     if PAYLOAD_CORRELATION_ID in event and PAYLOAD_S3_PREFIX in event:
         correlation_id = event[PAYLOAD_CORRELATION_ID]
         s3_prefix = event[PAYLOAD_S3_PREFIX]
-        snapshot_type = event[PAYLOAD_SNAPSHOT_TYPE]
+        snapshot_type = event.get(PAYLOAD_SNAPSHOT_TYPE, "full"))
         correlation_id_necessary = True
     elif "Records" in event:
         sns_message = event["Records"][0]["Sns"]
         payload = json.loads(sns_message["Message"])
         correlation_id = payload[PAYLOAD_CORRELATION_ID]
         s3_prefix = payload[PAYLOAD_S3_PREFIX]
-        snapshot_type = payload[PAYLOAD_SNAPSHOT_TYPE]
+        snapshot_type = payload.get(PAYLOAD_SNAPSHOT_TYPE, "full")
         correlation_id_necessary = True
 
     cluster_config = read_config("cluster")
