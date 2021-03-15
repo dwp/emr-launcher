0957088eef1d6dc23d8ad30d4ea572610aa547ca
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-03-15 11:44:01 +0000

    Fixup: Format Python code with Black

--- emr_launcher/tests/aws/test_aws.py
+++ emr_launcher/tests/aws/test_aws.py
@@ -5,8 +5,9 @@ from emr_launcher.aws import emr_cluster_add_tags
 import boto3
 
 from moto import mock_emr
-class TestConfig:
 
+
+class TestConfig:
     @mock_emr
     def test_emr_cluster_add_tags(self):
         emr_client = boto3.client("emr")
@@ -19,7 +20,6 @@ class TestConfig:
                 "MasterInstanceType": "c3.medium",
                 "Placement": {"AvailabilityZone": "eu-west-1"},
                 "SlaveInstanceType": "c3.xlarge",
-            }
+            },
         )
         print(resp)
-
