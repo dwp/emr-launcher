15de5c597f8e60450e845739bae2ed34d7123b60
ekgyimah <gyimah-emmy@hotmail.it>
2022-03-29 15:55:06 +0100

    Update tests

--- emr_launcher/tests/e2e/test_e2e.py
+++ emr_launcher/tests/e2e/test_e2e.py
@@ -129,7 +129,7 @@ class TestE2E:
         mock_retrieve_secrets.side_effect = mock_retrieve_secrets_side_effect
         handler({"correlation_id": "test", "s3_prefix": "test"})
 
-        assert mock_launch_cluster.call_count == 1
+        assert mock_launch_cluster.call_count == 0
         old_handler_call = mock_launch_cluster.call_args_list[0]
 
         handler(
